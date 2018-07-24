31a4cd3a9e9d2f4d3e62bc8a2d3d1dbec9f27d7a
Evans Musomi <evans.musomi@gmail.com>
2018-07-24 08:35:30 +0300

    feat: adds user query

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,9 +1,18 @@
 const graphql = require("graphql");
-const { GraphQLObjectType } = graphql;
+const { GraphQLObjectType, GraphQLString } = graphql;
+const UserType = require("./user_type");
 
 const RootQueryType = new GraphQLObjectType({
   name: "RootQueryType",
-  fields: {}
+  fields: {
+    user: {
+      type: UserType,
+      args: { email: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        console.log(args);
+      }
+    }
+  }
 });
 
 module.exports = RootQueryType;
