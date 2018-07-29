cbd39d22aead6e70fd8adcb141b054f66190d2e7
Evans Musomi <evans.musomi@gmail.com>
2018-07-29 05:48:29 +0300

    feat: adds logout mutation and handler

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
+import { Link } from "react-router";
 import query from "../queries/CurrentUser";
+import mutation from "../mutations/Logout";
 
 class Header extends Component {
+  onLogoutClick() {
+    this.props.mutate({
+      refetchQueries: [{ query }]
+    });
+  }
+
   renderButtons() {
     const { loading, user } = this.props.data;
 
@@ -11,19 +19,37 @@ class Header extends Component {
     }
 
     if (user) {
-      return <div>Logout</div>;
+      return (
+        <li>
+          <a onClick={this.onLogoutClick.bind(this)}>Logout</a>
+        </li>
+      );
     } else {
-      return <div>You're not signed in.</div>;
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      );
     }
   }
 
   render() {
     return (
       <nav>
-        <div className="nav-wrapper">{this.renderButtons()}</div>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo left">
+            Home
+          </Link>
+          <ul className="right">{this.renderButtons()}</ul>
+        </div>
       </nav>
     );
   }
 }
 
-export default graphql(query)(Header);
+export default graphql(mutation)(graphql(query)(Header));
