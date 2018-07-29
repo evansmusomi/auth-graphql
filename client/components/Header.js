e267625e23e6a632d8d8ed19993554f378f8094b
Evans Musomi <evans.musomi@gmail.com>
2018-07-29 05:29:29 +0300

    feat: adds authentication check to header and includes cookies with gql requests

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,9 +1,29 @@
 import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import query from "../queries/CurrentUser";
 
 class Header extends Component {
+  renderButtons() {
+    const { loading, user } = this.props.data;
+
+    if (loading) {
+      return <div />;
+    }
+
+    if (user) {
+      return <div>Logout</div>;
+    } else {
+      return <div>You're not signed in.</div>;
+    }
+  }
+
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">{this.renderButtons()}</div>
+      </nav>
+    );
   }
 }
 
-export default Header;
+export default graphql(query)(Header);
